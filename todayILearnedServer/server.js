562b5e9c654d4458e8cdee1c8729ef87d97f14c3
formal369 <73514392+formal369@users.noreply.github.com>
2022-05-18 20:16:39 +0900

    start making CRUD board

---
+++ todayILearnedServer/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const session = require('express-session');
+const connect = require('./schemas');
+
+connect();
+
+const corsOptions = {
+    origin: true,
+    credentials: true
+};
+
+app.use(
+    session({
+        resave: false,
+        saveUninitialized: true,
+        secret: "formal",
+        cookie: {
+            httpOnly: true,
+            secure: false
+        }
+    })
+);
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.listen(8080, () => {
+    console.log("listen good...")
+})
